f9549f37adca98c4cb711e2d1dcd9b9b5853c831
AramGevorgyan09 <gevorgyanaram0909@gmail.com>
2024-08-06 13:25:53 +0400

    changed

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
 import './styles/main.css'
+import './styles/adaptive.css'
 
 createApp(App).use(i18n).use(router).mount('#app')
